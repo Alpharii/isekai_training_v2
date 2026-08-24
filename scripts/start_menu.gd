4ca349ac12d38639b8e3bc2efcc1b3cd2123c9b7
alpharii <alphari404@gmail.com>
2026-08-24 22:17:41 +0700

    sesuatu

---
+++ scripts/start_menu.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _on_new_game_pressed() -> void:
+	print("test")
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+func _on_load_game_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")	
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
