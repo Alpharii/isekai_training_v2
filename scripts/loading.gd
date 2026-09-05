fdbf20c6fb0ee6bea4c07cf799634e50b6fc700c
alpharii <alphari404@gmail.com>
2026-09-05 18:47:04 +0700

    feat/enhance training tscene & script

--- scripts/loading.gd
+++ scripts/loading.gd
@@ -2,11 +2,10 @@ extends Node2D
 
 func _ready() -> void:
 	Dialogic.timeline_ended.connect(_on_timeline_ended)
-
-func _on_next_turn_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/training.tscn")
+	Dialogic.start("timeline")
 
 func _on_timeline_ended() -> void:
 	print("Timeline selesai!")
 	PlayerManager.player_data.last_completed_timeline = "timeline"
 	PlayerManager.save_game()
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -5,10 +5,10 @@ func _ready() -> void:
 
 func _on_new_game_pressed() -> void:
 	print("test")
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/loading.tscn")
 
 func _on_load_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")	
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
